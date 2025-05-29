b8330c4b78eeff0e2960ee2f901efb547e7dde3c
ericeeic <110375075+ericeeic@users.noreply.github.com>
2025-05-29 19:15:54 +0800

    Add files via upload

--- untitled3.py
+++ untitled3.py
@@ -1,31 +1,31 @@
 import streamlit as st 
 bi = st.button("HELLO")
 if bi == True:
-    st.image(r"C:\MARI\IMG_1314.gif")
+    st.image("IMG_1314.gif")
 sb1 = st.selectbox("CHOOSE",("MARI ANGRY","MARI SAD","MARI HAPPY","LICK"),index=None, placeholder="Select contact method...",)
 if sb1  == "MARI ANGRY":
-    st.image(r"C:\MARI\image0.jpg")
+    st.image("image0.jpg")
 if sb1 == "MARI SAD":
-    st.image(r"C:\MARI\IMG_0728.jpg")
+    st.image("IMG_0728.jpg")
 if sb1 == "MARI HAPPY":
-    st.image(r"C:\MARI\IMG_0977.jpg")
+    st.image("IMG_0977.jpg")
 if sb1 == "LICK":
-    st.image(r"C:\MARI\IMG_1314.gif")
+    st.image("IMG_1314.gif")
     
 te = st.text_input("MARI?")
 if te == "BREAK":
-    st.image(r"C:\MARI\imageh.jpg")
+    st.image("imageh.jpg")
 if te == "SHY":
-    st.image(r"C:\MARI\images.jpg")
+    st.image("images.jpg")
 if te == "CHM":
-    st.image(r"C:\MARI\imagefg.jpg")
+    st.image("imagefg.jpg")
 if te == "THREE":
-    st.image(r"C:\MARI\imagth.jpg")
+    st.image("imagth.jpg")
 if te == "SLEEP":
-    st.image(r"C:\MARI\2023-06-13_194233.png")
+    st.image("2023-06-13_194233.png")
 if te == "FOOD":
-    st.image(r"C:\MARI\IMG_5390.jpg")
+    st.image("IMG_5390.jpg")
 if te == "TAIL":
-    st.image(r"C:\MARI\IMG_2257.jpg")
+    st.image("IMG_2257.jpg")
 if te == "LICK":
-    st.image(r"C:\MARI\IMG_1314.gif")
+    st.image("IMG_1314.jpg")
